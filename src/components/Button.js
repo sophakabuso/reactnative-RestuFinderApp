e65ad4b8a600e718333dcb2825704725bf6d4dfd
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-17 13:00:45 +0200

    updatecomps

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const Button = ({ title, onPress, buttonStyle, textStyle }) => {
+    return (
+        <TouchableOpacity style={[styles.button, buttonStyle]} onPress={onPress}>
+            <Text style={[styles.text, textStyle]}>{title}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#007AFF',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default Button;
