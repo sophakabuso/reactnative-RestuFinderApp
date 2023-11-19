161bbdea2d20c6f807ab6de61cdf582d9427b131
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-19 14:04:23 +0200

    navigation

--- App.js
+++ App.js
@@ -1,36 +1,17 @@
-
+// App.js
 
 import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import store from './src/redux/store/store';
 
-import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import RestaurantScreen from './src/screens/RestaurantScreen';
-import MenuScreen from './src/screens/MenuScreen';
-import ReservationScreen from './src/screens/ReservationScreen';
-import ConfirmationScreen from './src/screens/ConfirmationScreen';
-import HistoryScreen from './src/screens/HistoryScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-
-const Stack = createStackNavigator();
+import AppNavigator from './src/navigation/AppNavigator'; // import AppNavigator
 
 const App = () => {
     return (
         <Provider store={store}>
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="Login">
-                    <Stack.Screen name="Login" component={LoginScreen} />
-                    <Stack.Screen name="Home" component={HomeScreen} />
-                    <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-                    <Stack.Screen name="Menu" component={MenuScreen} />
-                    <Stack.Screen name="Reservation" component={ReservationScreen} />
-                    <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
-                    <Stack.Screen name="History" component={HistoryScreen} />
-                    <Stack.Screen name="Profile" component={ProfileScreen} />
-                </Stack.Navigator>
+                <AppNavigator /> {/* use AppNavigator */}
             </NavigationContainer>
         </Provider>
     );
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,20 +1,31 @@
+// AppNavigator.js
+
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
-import DetailsScreen from '../screens/DetailsScreen';
+import RestaurantScreen from '../screens/RestaurantScreen';
+import MenuScreen from '../screens/MenuScreen';
+import ReservationScreen from '../screens/ReservationScreen';
+import ConfirmationScreen from '../screens/ConfirmationScreen';
+import HistoryScreen from '../screens/HistoryScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Details" component={DetailsScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen name="Menu" component={MenuScreen} />
+            <Stack.Screen name="Reservation" component={ReservationScreen} />
+            <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+            <Stack.Screen name="History" component={HistoryScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
     );
 };
 
