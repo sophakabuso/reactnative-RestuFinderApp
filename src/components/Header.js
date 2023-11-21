550d4271ca3f28aba744a6c67c361c09fcdeebd9
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-21 07:22:14 +0200

    update screens n Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import NetInfo from "@react-native-community/netinfo";
-import * as Battery from 'expo-battery';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
+
 
 const Header = () => {
     const [time, setTime] = useState(new Date().toLocaleTimeString());
@@ -21,7 +22,7 @@ const Header = () => {
                 setBatteryLevel(batteryLevel);
             } catch (error) {
                 console.error('Failed to fetch battery level:', error);
-            }
+            }Login 
         };
 
         fetchBatteryLevel();
@@ -37,9 +38,18 @@ const Header = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{time}</Text>
-            <Text style={styles.text}>{`Battery: ${Math.floor(batteryLevel * 100)}%`}</Text>
-            <Text style={styles.text}>{`Wifi: ${wifi ? 'On' : 'Off'}`}</Text>
-            <Text style={styles.text}>{`Network: ${network}`}</Text>
+            <View style={styles.iconContainer}>
+                <Icon name="battery" size={30} color="#900" /> {/* Use the Icon component */}
+                <Text style={styles.text}>{`Battery: ${Math.floor(batteryLevel * 100)}%`}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+                <Icon name="wifi" size={30} color="#900" /> {/* Use the Icon component */}
+                <Text style={styles.text}>{`Wifi: ${wifi ? 'On' : 'Off'}`}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+                <Icon name="globe" size={30} color="#900" /> {/* Use the Icon component */}
+                <Text style={styles.text}>{`Network: ${network}`}</Text>
+            </View>
         </View>
     );
 };
