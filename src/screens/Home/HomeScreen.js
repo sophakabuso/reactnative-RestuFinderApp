18c22d9d2314a50ca0b39f6b19248e406c137f13
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-28 09:44:11 +0200

    update home

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, ImageBackground, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Header from '../../components/Header';
@@ -7,9 +7,20 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [isUserRegistered, setIsUserRegistered] = useState(false); // Placeholder state
 
   const navigateToSignUp = () => {
     navigation.navigate('SignUp');
+  };
+  const onSubmit = () => {
+    // Check if the user is registered
+    if (isUserRegistered) {
+      // If registered, navigate to the list of featured restaurants
+      navigation.navigate('FeaturedRestaurants');
+    } else {
+      // If not registered, navigate to the sign-up screen
+      navigation.navigate('SignUp');
+    }
   };
 
   return (
@@ -29,9 +40,9 @@ const HomeScreen = () => {
 
             <View style={styles.welcostart}>
               <Text style={styles.welcomeback}>Dine and wine</Text>
-              <Text style={styles.getstarted}>with</Text>
-              <Text style={styles.welcomeback}>Friends and Family</Text>
-              <Text style={styles.getstarted}>Your Choice </Text>
+              <Text style={styles.with}>with</Text>
+              <Text style={styles.friends}>Friends and Family</Text>
+              <Text style={styles.choice}>Your Choice </Text>
             </View>
             <Image
               source={require('../../assets/images/decobarsmall.jpg')}
@@ -44,15 +55,23 @@ const HomeScreen = () => {
       <View style={styles.innerContainer}>
         <Image
           source={require('../../assets/images/homedish1.png')}
-          style={styles.homedish}
+          style={styles.homedish1}
         />
       </View>
 
       <View style={styles.innerContainer}>
         <Image
           source={require('../../assets/images/homedish2.png')}
-          style={styles.homedish}
+          style={styles.homedish2}
         />
+      </View>
+      <View>
+      <TouchableOpacity
+          style={styles.loginButton}
+          onPress={handleSubmit(onSubmit)}
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -82,7 +101,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 0,
+    padding: 20, // Add padding for better spacing
   },
   sobrestuContainer: {
     width: '100%',
@@ -90,53 +109,63 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 100,
+    paddingBottom: 20, // Adjust as needed
   },
   decobar: {
     width: 33.103,
     height: 7.207,
-    marginBottom: 10,
+    marginTop: 10, // Adjust for better spacing
+    marginBottom: 20, // Adjust for better spacing
   },
   sob: {
     color: '#FFF',
-    textAlign: 'center',
     fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: '900',
-    lineHeight: 18,
     letterSpacing: -0.165,
     marginLeft: 5,
   },
   restu: {
     color: '#FFF',
-    textAlign: 'center',
     fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: '900',
-    lineHeight: 18,
     letterSpacing: -0.165,
     marginLeft: 5,
   },
   welcostart: {
     flexDirection: 'column',
+    marginTop: 10, // Adjust for better spacing
   },
   welcomeback: {
     color: '#FFF',
-    textAlign: 'center',
     fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: '900',
-    lineHeight: 18,
     letterSpacing: -0.165,
     marginLeft: 5,
   },
-  getstarted: {
+  with: {
     color: '#FFF',
-    textAlign: 'center',
     fontFamily: 'Roboto',
     fontSize: 16,
     fontWeight: '900',
-    lineHeight: 18,
+    letterSpacing: -0.165,
+    marginLeft: 5,
+  },
+  friends: {
+    color: '#FFF',
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: '900',
+    letterSpacing: -0.165,
+    marginLeft: 5,
+  },
+  choice: {
+    color: '#FFF',
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: '900',
     letterSpacing: -0.165,
     marginLeft: 5,
   },
@@ -148,11 +177,19 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#0F0101',
   },
-  homedish: {
+  homedish1: {
     width: 200, // Adjust the width as needed
     height: 200, // Adjust the height as needed
     resizeMode: 'cover',
+    marginBottom: 20, // Adjust for better spacing
+  },
+  homedish2: {
+    width: 100, // Adjust the width as needed
+    height: 10, // Adjust the height as needed
+    resizeMode: 'cover',
+    marginBottom: 20, // Adjust for better spacing
   },
 });
+
 
 export default HomeScreen;
